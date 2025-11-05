3e04869188e875f7731139ccd4da22cdb2fac18c
Paryx <paryx.games@gmail.com>
2025-11-05 18:44:14 +0000

    feat: added entire project src at this time

---
+++ build/build-linux.bat
@@ -0,0 +1,12 @@
+@echo off
+echo Building Pulse Media...
+npx electron-packager ../ "Pulse Media" --platform=linux --arch=x64 --out=../dist --overwrite
+
+    echo.
+    echo Build failed!
+    pause
+    exit /b
+)
+echo.
+echo Build complete! Press Enter to exit.
+pause >nul
---
+++ src/renderer/js/contextMenu.js
@@ -0,0 +1,2 @@
+// Context menu is now disabled in favor of cleaner UI
+// Right-click is allowed for standard browser context menu
